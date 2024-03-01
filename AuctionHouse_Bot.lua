b2f2d3597777473f9692e4360246b73fc5ebdc2f
Dhiyo Ketut Crisnawidhi <crisnawidhi_009@yahoo.com>
2024-03-01 16:27:43 +0800

    new: adding image API from Sinmeowgra instead of using dbc data, remaking the discord send method and more!

--- AuctionHouse_Bot.lua
+++ AuctionHouse_Bot.lua
@@ -1,53 +1,65 @@
 -- Credits Mithras, Niam5, and ReynoldsCahoon --
 
--- TODO: Include a proper JSON library
+-- Remastered by Jacatinelord / Sinmeowgra For Image API --
+-- EMOJI ID Tutorial --
+-- To add, go to your server settings -> emoji -> add 3 emojis to represent gold/silver/copper
+-- Once added, in any channel type your emoji preceded by a backslash \ i.e. "\:gold:" -- This will give you the emoji id, which you place in these values i.e. "<:gold:1209645132832575578>"
+-- END OF EMOJI ID Tutorial --
 
+-- TODO: Include a proper JSON library
 local auctionConfig = {
-    auctionWebhookURL = "YOUR_WEBHOOK_URL",     -- Your Discord channel webhook url
-    goldEmojiID       = "YOUR_GOLD_EMOJI_ID",   -- Your gold/silver/copper emojis
-    silverEmojiID     = "YOUR_SILVER_EMOJI_ID",     -- To add, go to your server settings -> emoji -> add 3 emojis to represent gold/silver/copper
-    copperEmojiID     = "YOUR_COPPER_EMOJI_ID",     -- Once added, in any channel type your emoji preceded by a backslash \ i.e. "\:gold:"
-                                                    -- This will give you the emoji id, which you place in these values i.e. "<:gold:1209645132832575578>"
-    itemLinkDB        = "YOUT_ITEM_LINK_DB",    -- Your own AoWoW instance, or wowhead i.e. "https://www.wowhead.com/wotlk/"
-    thumbnailIcons    = true                    -- Set to false if you don't have DBC data in your database and wish to forego icon thumbnails
-    itemIconDB        = "YOUR_ITEM_ICON_DB",    -- A web-accessible directory of all icon images, or wowhead i.e. "https://wow.zamimg.com/images/wow/icons/large/"
-    botImage          = "YOUR_BOT_IMAGE_URL"    -- The profile picture of the bot that posts in Discord
-    itemQuality       = {
-                  [0] = {color = 10329501, name = "Poor"},
-                  [1] = {color = 16777215, name = "Common"},
-                  [2] = {color = 2031360, name = "Uncommon"},
-                  [3] = {color = 28893, name = "Rare"},
-                  [4] = {color = 10696174, name = "Epic"},
-                  [5] = {color = 16744448, name = "Legendary"},
-                  [6] = {color = 15125632, name = "Artifact"},
-                  [7] = {color = 52479, name = "Heirloom"}
+    auctionWebhookURL = "", -- Your Discord channel webhook url
+    goldEmojiID = "", -- Your gold/silver/copper emojis
+    silverEmojiID = "",
+    copperEmojiID = "", 
+    itemLinkDB = "https://wowgaming.altervista.org/aowow/", -- Your own AoWoW instance, or wowhead i.e. "https://www.wowhead.com/wotlk/"
+    thumbnailIcons = true, -- Set to false if you don't have DBC data in your database and wish to forego icon thumbnails
+    itemIconDB = "https://wowgaming.altervista.org/aowow/static/images/wow/icons/large/", -- A web-accessible directory of all icon images, or wowhead i.e. "https://wow.zamimg.com/images/wow/icons/large/"    -- The profile picture of the bot that posts in Discord
+    itemQuality = {
+        [0] = {
+            color = 10329501,
+            name = "Poor"
+        },
+        [1] = {
+            color = 16777215,
+            name = "Common"
+        },
+        [2] = {
+            color = 2031360,
+            name = "Uncommon"
+        },
+        [3] = {
+            color = 28893,
+            name = "Rare"
+        },
+        [4] = {
+            color = 10696174,
+            name = "Epic"
+        },
+        [5] = {
+            color = 16744448,
+            name = "Legendary"
+        },
+        [6] = {
+            color = 15125632,
+            name = "Artifact"
+        },
+        [7] = {
+            color = 52479,
+            name = "Heirloom"
+        }
     }
 }
-
-local function SendDiscordEmbed(message, webhookURL)
-    local curlCommand = 'curl -X POST -H "Content-Type: application/json" -d @- '..webhookURL
-    local curlProcess = io.popen(curlCommand, 'w')
-    curlProcess:write(message)
-    curlProcess:close()
-end
-
-local function GetIconFromDBC(itemId)
-    if auctionConfig.thumbnailIcons then
-        local icon = WorldDBQuery("SELECT InventoryIcon_1 FROM itemdisplayinfo_dbc WHERE ID = (SELECT displayid FROM item_template WHERE entry = "..itemId..")");
-        return auctionConfig.itemIconDB..icon:GetString(0):lower()..'.jpg'
-    else
-        return ""
-    end
-end
+local gender
 
 local function GetPlayerIcon(player)
     local race = string.gsub(player:GetRaceAsString(), " ", ""):lower()
     if player:GetGender() == 1 then
-        local gender = "female"
+        gender = "female"
     else
-        local gender = "male"
+        gender = "male"
     end
-    return auctionConfig.itemIconDB..'race_'..race..'_'..gender..'.jpg'
+    return auctionConfig.itemIconDB .. 'race_' .. race .. '_' .. gender .. '.jpg'
 end
 
 local function EscapeQuotes(text)
@@ -82,7 +94,10 @@ end
 
 local function OnAuctionAdd(event, auctionId, owner, item, expireTime, buyout, startBid, currentBid, bidderGUIDLow)
 
-    bodyStart = string.format('{"content": null, "embeds": [{"title": "%s", "description": "%s", "url": "%s", "color": %d, "fields": [', EscapeQuotes(item:GetName()), ' '..item:GetItemLevel(), auctionConfig.itemLinkDB..'item='..tostring(item:GetEntry()), auctionConfig.itemQuality[item:GetQuality()].color)
+    bodyStart = string.format(
+        '{"content": null, "type": "rich", "embeds": [{"title": "%s", "description": "Item Level: %s", "url": "%s", "color": %d, "fields": [',
+        item:GetName(), ' ' .. item:GetItemLevel(), auctionConfig.itemLinkDB .. 'item=' .. tostring(item:GetEntry()),
+        auctionConfig.itemQuality[item:GetQuality()].color)
 
     fields = ""
 
@@ -95,12 +110,19 @@ local function OnAuctionAdd(event, auctionId, owner, item, expireTime, buyout, s
 
     fields = fields .. string.format('{"name": "Selling %d for","value": "%s"}', item:GetCount(), listing)
 
-    bodyEnd = string.format('],"author": {"name": "%s listed", "icon_url": "%s"}, "footer": {"text": "Listed until"}, "timestamp": "%s", "thumbnail": {"url":"%s"}}], "username": "%s", "avatar_url": "%s", "attachments": []}', owner:GetName(), GetPlayerIcon(owner), os.date("!%Y-%m-%dT%X.000Z", expireTime), GetIconFromDBC(item:GetEntry()), "Auction House", auctionConfig.botImage)
+    bodyEnd = string.format(
+        '],"author": {"name": "%s listed", "icon_url": "%s"}, "footer": {"text": "Listed until"}, "timestamp": "%s", "thumbnail": {"url":"https://sinmeowgra.com/api/ah-lua.php?itemid=%i"}}], "username": "Auction House", "attachments": []}',
+        owner:GetName(), GetPlayerIcon(owner), os.date("!%Y-%m-%dT%X.000Z", expireTime), item:GetEntry())
 
     message = bodyStart .. fields .. bodyEnd
 
     -- Send the message to Discord
-    SendDiscordMessage(message, auctionConfig.auctionWebhookURL)
+    print(message)
+    HttpRequest("POST", auctionConfig.auctionWebhookURL, message, "application/json", function(status, body, headers)
+        if status ~= 200 then
+            print("DiscordNotifier[Lua] Error when sending webhook to discord. Response body is: " .. body)
+        end
+    end)
 end
 
 -- Register the event handler for AUCTION_EVENT_ON_ADD
